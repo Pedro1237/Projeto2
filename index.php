7c045fe75c9231e9f0794be8a8024fab8a366b6a
Pedro1237 <105647384+Pedro1237@users.noreply.github.com>
2023-11-24 21:57:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar Reserva</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
 </head>
-
 <body>
     <form action="cadastrar.php" method="POST" class="m-6">
-        <div class="field">
-            <label class="label">Id</label>
-            <div class="control">
-                <input class="input" type="text" name="txtId">
-            </div>
-        </div>
         <div class="field">
             <label class="label">Nome</label>
             <div class="control">
@@ -23,27 +15,27 @@
             </div>
         </div>
         <div class="field">
-            <label class="label">Checkin</label>
+            <label class="label">Check-in</label>
             <div class="control">
-                <input class="input" type="text" name="txtChekin">
+                <input class="input" type="text" name="txtCheckin">
             </div>
         </div>
         <div class="field">
-            <label class="label">Checkout</label>
+            <label class="label">Check-out</label>
             <div class="control">
                 <input class="input" type="text" name="txtCheckout">
             </div>
         </div>
         <div class="field">
-            <label class="label">Número de Hospedes</label>
+            <label class="label">Número de Hóspedes</label>
             <div class="control">
-                <input class="input" type="text" name="txtHospedes">
+                <input class="input" type="text" name="txtNumHospedes">
             </div>
         </div>
 
         <button type="submit" class="button is-primary">Cadastrar</button>
+        <a class="button is-warning" href="listar.php">Listar dados</a>
+        <a class="button is-info" href="index.html">Ver Pousadas</a>
     </form>
-    <a class="button is-warning" href="listar.php">Listar Reservas</a>
 </body>
-
 </html>
